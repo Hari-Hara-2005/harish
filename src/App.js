85edf2f99757a268305c8a5a272ac4e420f9891d
Hari-Hara-2005 <gharanthasec@gmail.com>
2024-07-20 18:40:27 +0530

    first

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react'
 import {BrowserRouter,Routes,Route,Navigate} from 'react-router-dom'
 import Signup from './Component/Signup'
 import Register from './Component/Register'
-import Home from './Component/Home'
+
 
 const App = () => {
   return (
     <BrowserRouter>
       
     <Routes>
-      <Route path='/' element={<Home/>}></Route>
+      <Route path='/' element={<Register/>}></Route>
       <Route path='/signup' element={<Signup/>}/>
       <Route path='/register' element={<Register/>}/>
         <Route path='*' element={<Navigate to="/" />} />
